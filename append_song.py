ba5ca110f6a0937fb0b6970a83cb34bd169684f2
grei <vahfan@gmail.com>
2023-09-01 22:54:56 +1200

    automated

--- append_song.py
+++ append_song.py
@@ -4,25 +4,27 @@ import webbrowser
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy import Spotify
 
-from constants import CREDENTIALS_PATH
+from constants import MICRODOSING_URI, get_credentials, SCOPE
+from scrape import scrape
 
 
 
 def main():
 
-
+    credentials = get_credentials()
 
     # Initialize the Spotify OAuth object
-    sp_oauth = SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=SCOPE)
+    sp_oauth = SpotifyOAuth(client_id=credentials.id, client_secret=credentials.secret, redirect_uri=credentials.redirect_uri, scope=SCOPE)
 
     auth_url = sp_oauth.get_authorize_url()
-    webbrowser.open(auth_url)
+    auth_code = scrape(auth_url)
 
-    # Wait for the user to authorize the app and enter the code
-    authorization_code = input("Enter the authorization code: ")
+    # access webpage instead (user interaction)
+    # webbrowser.open(auth_url)
+    # authorization_code = input("Enter the authorization code: ")
 
     # Exchange authorization code for access token
-    token_info = sp_oauth.get_access_token(authorization_code)
+    token_info = sp_oauth.get_access_token(auth_code)
 
     # Extract the access token
     access_token = token_info['access_token']
@@ -30,9 +32,15 @@ def main():
     # Now you can use the access token to make API requests
     sp = Spotify(auth=access_token)
 
-    # Example: Get the user's saved tracks
-    results = sp.add_to_queue(MICRODOSING_URI)
 
+    # need to check if the user is active
+
+    # check the current song
+
+    # loop through dictionary of song pairings
+
+    # add to queue a song if there's a match
+    sp.add_to_queue(MICRODOSING_URI)
 
 if __name__ == '__main__':
     main()
---
+++ scrape.py
@@ -0,0 +1,34 @@
+import json
+from typing import Optional
+import requests
+from bs4 import BeautifulSoup
+
+# URL of the webpage you want to scrape
+URL = 'http://127.0.0.1:5000/'
+
+def scrape(url: str) -> Optional[str]:
+    # Send an HTTP GET request to the URL
+    response = requests.get(url or URL)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the HTML content of the page using BeautifulSoup
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Find the <meta> tag with id="bootstrap-data"
+        meta_tag = soup.find('meta', id='bootstrap-data')
+
+        # Extract the content attribute of the <meta> tag
+        bootstrap_data = meta_tag['sp-bootstrap-data']
+
+        # Parse the content as JSON
+        bootstrap_data_json = json.loads(bootstrap_data)
+
+        # Access the "tpaState" value
+        tpa_state_value = bootstrap_data_json.get('tpaState')
+
+        # Print the "tpaState" value
+        return tpa_state_value
+
+    print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+    return None
